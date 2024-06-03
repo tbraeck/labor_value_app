b5f5edb91b998667f469f7011893fef80802a266
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-06-02 20:29:55 -0600

    background iamge home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,49 @@
 import SearchAppBar from "./_components/SearchAppBar";
-import ImageBackgroundMain from "./_components/ImageBackgroundMain";
+import Image from "next/image";
+import { Box } from '@mui/material';
 
 export default function Home() {
-  return (
-    <>
-    <ImageBackgroundMain>
-    <SearchAppBar/>
+  const imageUrl = "./assets/working.jpg";
 
-    </ImageBackgroundMain>
-    </>
-   
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        height: '500px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+        }}
+      >
+        <Image
+          layout="fill"
+          src="/working.jpg"
+          alt="Image alt"
+          style={{ objectFit: "cover" }}
+        />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: 'linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
+          }}
+        />
+      </Box>
+      <SearchAppBar />
+    </Box>
   );
 }
