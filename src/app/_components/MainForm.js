71ea3f75652eceeb013893046bf34bb71795cab1
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-06-03 17:19:45 -0600

    Built Mainform and ADjusting for home

---
+++ src/app/_components/MainForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container } from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
+const MainForm = () => {
+  return (
+    <div>
+      <Container maxWidth="sm">
+    <Box
+     sx={{width: 500,
+     height: 500,
+     borderRadius: 7,
+     bgcolor: 'primary.main',
+     marginTop:20,
+     }}>
+      <div>
+      <h1>
+    
+       </h1>
+      </div>
+    
+    </Box>
+    </Container>
+    </div>
+  )
+}
+
+export default MainForm
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
 import SearchAppBar from "./_components/SearchAppBar";
+import MainForm from "./_components/MainForm";
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
+
 import './styles/globals.css';
 
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen bg-cover bg-center" style={{ backgroundImage: "url('../public/_assets/working.jpg')" }}>
+    <div className="relative h-screen w-screen bg-cover bg-center" style={{ backgroundImage: "url('../public/_assets/working.jpg')", backgroundRepeat: "no-repeat", }}>
     <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
+    <CssBaseline/>
     <Box className="relative z-10 w-full">
       <SearchAppBar />
     </Box>
+    <MainForm/>
+    
+    
   </div>
 
   );
