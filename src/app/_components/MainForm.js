f02ef5d64bf1d4df40be4f526cc5f07e5048d1c9
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-06-11 16:33:18 -0600

    Trying to get api fired up

--- src/app/_components/MainForm.js
+++ src/app/_components/MainForm.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect } from "react";
 import Autocomplete from '@mui/material/Autocomplete';
@@ -12,108 +12,113 @@ const MainForm = () => {
     jobName: "",
     yearsExp: '',
     zipCode: "",
-};
+  };
 
-    const [formData, setFormData] = useState(initialFormData);
-    const [jobOptions, setJobOptions] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
+  const [jobOptions, setJobOptions] = useState([]);
 
-    useEffect(() => {
-        fetch('.data.json') 
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-                setJobOptions(data); 
-            })
-            .catch((error) => {
-                console.error("Error fetching job data:", error);
-            });
-    }, []);
+  useEffect(() => {
+    fetch('https://retoolapi.dev/6dI6Gb/data')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setJobOptions(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching job data:", error);
+      });
+  }, []);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleReset = (e) => {
-      e.preventDefault();
-      setFormData(initialFormData)
+  const handleReset = (e) => {
+    e.preventDefault();
+    setFormData(initialFormData);
+  };
 
-    }
+  const handleAutocompleteChange = (event, newValue) => {
+    setFormData({ ...formData, jobName: newValue });
+  };
 
-    const handleAutocompleteChange = (event, newValue) => {
-        setFormData({ ...formData, jobName: newValue });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData.jobName);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-      console.log(formData.jobName)
-    };
-
-    return (
-      <Box>
-        <Box>
-          <Image src="Scale.gif" height="150" width="150" alt="Scale" />
-        </Box>
-        <form className="form" onSubmit={handleSubmit}>
-            <label htmlFor="jobName" className="label">
-                JOB NAME
-            </label>
-            <Autocomplete
-                freeSolo
-                id="jobName"
-                options={jobOptions.map((option) => option.jobName)} // Assuming each job has a jobName field
-                value={formData.jobName}
-                onChange={handleAutocompleteChange}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        name="jobName"
-                        variant="outlined"
-                        placeholder="Laborer"
-                        className="input"
-                        onChange={handleChange}
-                    />
-                )}
-            />
-
-            <label htmlFor="yearsExp" className="label">
-                YEARS OF EXPERIENCE
-            </label>
+  return (
+    <Box>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="formImg">
+          <Image src="/Scale.gif" height="150" width="150" alt="Scale" />
+        </div>
+        <label htmlFor="jobName" className="label">
+          JOB NAME
+        </label>
+        <Autocomplete
+          freeSolo
+          id="jobName"
+          options={jobOptions.map((option) => option.jobName)} // Assuming each job has a jobName field
+          value={formData.jobName}
+          onChange={handleAutocompleteChange}
+          renderInput={(params) => (
             <TextField
-                required
-                type="number"
-                id="yearsExp"
-                name="yearsExp"
-                placeholder="Years of Experience"
-                value={formData.yearsExp}
-                onChange={handleChange}
-                className="input"
-                variant="outlined"
+              {...params}
+              name="jobName"
+              variant="outlined"
+              placeholder="Laborer"
+              className="input"
+              onChange={handleChange}
             />
+          )}
+        />
 
-            <label htmlFor="zipCode" className="label">
-                ZIP CODE
-            </label>
-            <TextField
-                required
-                id="zipCode"
-                name="zipCode"
-                placeholder="Zip Code"
-                value={formData.zipCode}
-                onChange={handleChange}
-                className="input"
-                variant="outlined"
-            />
+        <label htmlFor="yearsExp" className="label">
+          YEARS OF EXPERIENCE
+        </label>
+        <TextField
+          required
+          type="number"
+          id="yearsExp"
+          name="yearsExp"
+          placeholder="Years of Experience"
+          value={formData.yearsExp}
+          onChange={handleChange}
+          className="input"
+          variant="outlined"
+        />
 
-            <Button type="submit" onSubmit={handleSubmit} className="button" variant="contained" color="primary">
-                Submit
-            </Button>
-            <Button type="reset" onClick={handleReset}  className="button" variant="outlined" color="secondary">
-                Reset
-            </Button>
-        </form>
-        </Box>
-    );
+        <label htmlFor="zipCode" className="label">
+          ZIP CODE
+        </label>
+        <TextField
+          required
+          id="zipCode"
+          name="zipCode"
+          placeholder="Zip Code"
+          value={formData.zipCode}
+          onChange={handleChange}
+          className="input"
+          variant="outlined"
+        />
+
+        <Button type="submit" onSubmit={handleSubmit} className="button" variant="contained" color="primary">
+          Submit
+        </Button>
+        <Button type="reset" onClick={handleReset} className="button" variant="outlined" color="secondary">
+          Reset
+        </Button>
+      </form>
+    </Box>
+  );
 };
 
 export default MainForm;
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import SearchAppBar from "./_components/SearchAppBar";
 import Head from "next/head";
-
-// import ImgMediaCard from './_components/ImgMediaCard';
-// import ValueForm from './_components/ValueForm'; 
 import Box from '@mui/material/Box';
 import MainForm from './_components/MainForm';
 
