45503daacb67cdce707a593e3029f224a96d206d
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-24 15:40:05 -0500

    adding routes for different page links

---
+++ src/app/contact/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <form>
+        <label>Contact US</label>
+        <input 
+        type="text" 
+        name="contact US" 
+        placeholder="Contact US">
+            
+        </input>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmFzbmh2NmE5cmFzaDZqcmoxc3BtYTYzaDlsbmZhZnY5OTNhbTEyNiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/SggILpMXO7Xt6/200.webp' alt=''/>
+    </div>
+  )
+}
+
+export default Home
